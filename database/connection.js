32cafbf2cb164be25dac549a39890a9288ce81cf
jmcsuite <jmcsuite@hotmail.com>
2021-10-23 22:28:37 -0500

    guardado temportal

---
+++ database/connection.js
@@ -0,0 +1,9 @@
+let a = 44;
+
+function getCredentials() {
+    console.log(a);
+    return a;
+}
+
+// exports
+module.exports = { getCredentials };
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 //  Mongoose / SQL 
-
+const database_connection = require("./database/connection.js");
+pp = database_connection.getCredentials();
+console.log(` ajaja ${pp}`)
 
 
 // //  Express
@@ -15,3 +17,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
