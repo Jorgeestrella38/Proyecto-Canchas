20b415ae3e76eccdbe6b1fa8df9c90e29add899c
AlanSauqui <0227221@up.edu.mx>
2021-10-26 12:14:39 -0500

    Formato ejs y conexiones entre ejs hechas
    Se agregaron ejs de todas las páginas con un formato establecido. Se creó el archivo head.ejs con los encabezados en forma general (ya no se ocupa agregar bootstrap ni las etiquetas del comienzo) y a cada archivo se le adjunto un vínculo con el navbar.ejs. Además, ahora todas las páginas tienen su función hecha y funcional en el server.js.

--- server.js
+++ server.js
@@ -1,17 +1,66 @@
-/*jshint esversion: 6 */ 
-// SQL 
+// -----------------------SQL -----------------------
 const dbUtils = require("./database/connection.js");
 const  dbConnection = dbUtils.createConnection();
 
-// express
-const express = require('express');
-const app = express();
-const port = 3000;
 
+
+
+// -----------------------Express-----------------------
+var express = require('express');
+const path = require('path');
+var app = express();
+  
+// Set EJS as templating engine 
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname));
+  
+// Variable de prueba teemporal
+const userInfo = 
+    {
+        name: "",
+        email: ""
+    }
+
+
+// -----------------------Functions-----------------------
+// Pagina de prueba con todos los links
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.render('pages/home', { userInfo: userInfo });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+// Home
+app.get('/Inicio', (req, res) => {
+    res.render('pages/home', { userInfo: userInfo });
+});
+
+// Reservaciones
+app.get('/Reservaciones', (req, res) => {
+    res.render('pages/reservations', { userInfo: userInfo });
+});
+
+// Login
+app.get('/IniciarSesion', (req, res) => {
+    res.render('pages/login', { userInfo: userInfo });
+});
+
+// Register
+app.get('/Registrarse', (req, res) => {
+    res.render('pages/register', { userInfo: userInfo });
+});
+
+// How to reserve
+app.get('/ComoReservar', (req, res) => {
+    res.render('pages/howToReserve', { userInfo: userInfo });
+});
+
+// More info
+app.get('/MasInfo', (req, res) => {
+    res.render('pages/moreInfo', { userInfo: userInfo });
+});
+  
+// Server setup
+app.listen(3000, function (req, res) {
+    console.log("Connected on port:3000");
 });
